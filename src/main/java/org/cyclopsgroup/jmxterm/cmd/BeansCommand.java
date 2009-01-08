f3f25a7354522a20c7f4db9b2d8fbe92d46e8f41
jqguo <jqguo@52cbe8be-5010-0410-be98-a0f1afd79c3a>
2009-01-08 05:03:15 +0000

    Fix user manual

--- src/main/java/org/cyclopsgroup/jmxterm/cmd/BeansCommand.java
+++ src/main/java/org/cyclopsgroup/jmxterm/cmd/BeansCommand.java
@@ -34,7 +34,6 @@ public class BeansCommand
      * @throws MalformedObjectNameException
      * @throws IOException
      */
-    @SuppressWarnings( "unchecked" )
     public static List<String> getBeans( Session session, String domainName )
         throws MalformedObjectNameException, IOException
     {
